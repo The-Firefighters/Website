582e2a0905c1ac90074191033b9cc0aa21586ef5
20shaked20 <shakedlevi32@gmail.com>
2024-08-10 05:03:19 +0300

    Minor change to toolbar to stick to the top when dragging + redesgin the home page with dropdown for better looks

--- frontend/src/componenets/Home/HomeScreen.js
+++ frontend/src/componenets/Home/HomeScreen.js
@@ -1,53 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './HomeScreen.css';
 
 function HomeScreen() {
-  const TheProblem = "Explanation about the problem and who created the solutions";
-  const Spreading = "The Spreading methode says that the vaccination can spread from the nodes to their neighbors just like the infection/fire";
-  const NonSpreading = "The Spreading methode says that the vaccination cannot spread from the nodes to their neighbors just like the infection/fire, meaning that the vaccination is static";
-  const MaxSave = "In the MaxSave algorithm, giving a certain budget, we need to save as many nodes that we can from the targeted nodes";
-  const MinBudget = "In the MinBudget algorithm, we need to find the minimal budget that will save all the targeted nodes"; 
+  const [activeDropdowns, setActiveDropdowns] = useState({});
+
+  const toggleDropdown = (id) => {
+    setActiveDropdowns(prev => ({
+      ...prev,
+      [id]: !prev[id]
+    }));
+  };
+
+  const dropdowns = [
+    {
+      id: 'problem',
+      title: 'The Problem',
+      content: "Explanation about the problem and who created the solutions"
+    },
+    {
+      id: 'spreading',
+      title: 'The Spreading method',
+      content: "The Spreading methode says that the vaccination can spread from the nodes to their neighbors just like infection/fire"
+    },
+    {
+      id: 'nonSpreading',
+      title: 'The Non-Spreading method',
+      content: "The Spreading methode says that the vaccination cannot spread from the nodes to their neighbors just like infection/fire, meaning that the vaccination is static"
+    },
+    {
+      id: 'maxSave',
+      title: 'The MaxSave algorithm',
+      content: "In the MaxSave algorithm, giving a certain budget, we need to save as many nodes that we can from the targeted nodes"
+    },
+    {
+      id: 'minBudget',
+      title: 'The MinBudget algorithm',
+      content: "In the MinBudget algorithm, we need to find the minimal budget that will save all the targeted nodes"
+    }
+  ];
 
   return (
     <div className="home-screen">
       <div className="input-container">
-      <h2 style={{ color: 'white' }}>The Problem</h2>
-        <div className="text-box">
-          {TheProblem}
-        </div>
+        <h2>The Firefighter Problem</h2>
         
-        <h3>We have two methods for the firefighter problem:</h3>
-        <div className="method-container">
-          <div className="method">
-            <h4>The Spreading methode</h4>
-            <div className="text-box">
-              {Spreading}
+        {dropdowns.map((dropdown, index) => (
+          <React.Fragment key={dropdown.id}>
+            {index === 1 && <h3>We have two methods for the firefighter problem:</h3>}
+            {index === 3 && <h3>For each method there are two types of algorithms:</h3>}
+            <div className="dropdown">
+              <div 
+                className={`dropdown-header ${activeDropdowns[dropdown.id] ? 'active' : ''}`}
+                onClick={() => toggleDropdown(dropdown.id)}
+              >
+                <h4>{dropdown.title}</h4>
+              </div>
+              <div className={`dropdown-content ${activeDropdowns[dropdown.id] ? 'active' : ''}`}>
+                <p>{dropdown.content}</p>
+              </div>
             </div>
-          </div>
-          <div className="method">
-            <h4>The Non-Spreading methode</h4>
-            <div className="text-box">
-              {NonSpreading}
-            </div>
-          </div>
-        </div>
-        
-        <h3>For each methode there are two types of algorithms:</h3>
-        <div className="algorithm-container">
-          <div className="algorithm">
-            <h4>The MaxSave algorithm</h4>
-            <div className="text-box">
-              {MaxSave}
-            </div>
-          </div>
-          <div className="algorithm">
-            <h4>The MinBudget algorithm</h4>
-            <div className="text-box">
-              {MinBudget}
-            </div>
-          </div>
-        </div>
+          </React.Fragment>
+        ))}
         
         <Link to="/AlgorithmsPage" className="try-it-out-button">
           Try It Out!
