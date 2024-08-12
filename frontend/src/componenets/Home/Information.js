41c4f67214cbebb08ae623e4903834682ff844ae
Almog-David <almog.david99@gmail.com>
2024-08-12 16:59:46 +0300

    starting the information page. need to work on latex

--- frontend/src/componenets/Home/Information.js
+++ frontend/src/componenets/Home/Information.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Latex from 'react-latex'
 import './Information.css';
 
 function Information() {
   const [activeDropdowns, setActiveDropdowns] = useState({});
+
+const SpreadingMaxsaveCode = `$$
+\\begin{algorithm}[H]
+\\SetAlgoLined
+\\KwIn{Graph $G(V,E)$, source node $s$, target set $T \\subseteq V$}
+\\KwOut{The approximated minimal budget $B$}
+Perform a binary search on the target group $T$\\;
+Run the MaxSave algorithm on the parameters of the problem with the budget $B$ that we found in the binary search\\;
+Match the budget $B$ so that it is minimal but still able to save all nodes of $T$\\;
+\\Return{$B$}
+\\caption{($\\ln n$)-approx algorithm}
+\\end{algorithm}
+$$`;
 
   const toggleDropdown = (id) => {
     setActiveDropdowns(prev => ({
@@ -12,52 +26,71 @@ function Information() {
     }));
   };
 
+  const formatContent = (content) => {
+    return content.split(/([.:])/).map((part, index) => 
+      index % 2 === 0 ? part : <React.Fragment key={index}>{part}<br /></React.Fragment>
+    );
+  };
+
   const dropdowns = [
     {
-      id: 'problem',
-      title: 'Test',
-      content: "bla bla bla"
+      id: 'Spreading-Maxsave',
+      title: 'Spreading MaxSave',
+      content: "content",
+      latex: SpreadingMaxsaveCode
     },
     {
-      id: 'spreading',
-      title: 'The Spreading method',
-      content: "The Spreading methode says that the vaccination can spread from the nodes to their neighbors just like infection/fire"
+      id: 'Spreading-Minbudget',
+      title: 'Spreading Minbudget',
+      content: "In the Spreading MinBudget algorithm, the MinBudget problem is as hard as a set-cover. There is a set-cover of size B if and only if there is a vaccination strategy immunizing at most B nodes per time step. Therefore the algrotithm will be: ",
+      latex: ""
     },
     {
-      id: 'nonSpreading',
-      title: 'The Non-Spreading method',
-      content: "The Spreading methode says that the vaccination cannot spread from the nodes to their neighbors just like infection/fire, meaning that the vaccination is static"
+      id: 'NonSpreading-Minbudget',
+      title: 'Non-Spreading Minbudget',
+      content:"For the Non-Spreading Minbudget algorithm, the article considers the following equivalent problem: We create a new node t and edges from all nodes in T (the target node's group) to t, and consider the problem of saving t with the minimum budget, under the additional constraint that t itself cannot be vaccinated. Call s the source and t the sink. Therefore the algorithm will be:",
+      latex: ""
     },
     {
-      id: 'maxSave',
-      title: 'The MaxSave algorithm',
-      content: "In the MaxSave algorithm, giving a certain budget, we need to save as many nodes that we can from the targeted nodes"
+      id: 'NonSpreading-Dirlay-Minbudget',
+      title: 'Non-Spreading Dirlay Minbudget',
+      content: ".", 
+      latex: ""
     },
     {
-      id: 'minBudget',
-      title: 'The MinBudget algorithm',
-      content: "In the MinBudget algorithm, we need to find the minimal budget that will save all the targeted nodes"
+      id: 'Heuristic-Maxsave',
+      title: 'Heuristic Maxsave',
+      content: ".",
+      latex: ""
+
+    },
+    {
+      id: 'Heuristic-Minbudget',
+      title: 'Heuristic Minbudget',
+      content: "We will use in the same approach, just like in the Spreading MinBudget algorithm. Therefore the algorithm will be: ", 
+      latex: ""
+
     }
   ];
 
   return (
     <div className="information-page">
       <div className="input-container">
-        <h2>Information</h2>
-        
+        <h3>About The Algorithms:</h3>
+
         {dropdowns.map((dropdown, index) => (
           <React.Fragment key={dropdown.id}>
-            {index === 1 && <h3>test:</h3>}
-            {index === 3 && <h3>test2:</h3>}
+            {index === 4 && <h3>The Heuristic Algorithms:</h3>}
             <div className="dropdown">
               <div 
                 className={`dropdown-header ${activeDropdowns[dropdown.id] ? 'active' : ''}`}
                 onClick={() => toggleDropdown(dropdown.id)}
               >
-                <h4>{dropdown.title}</h4>
+              <h4>{dropdown.title}</h4>
               </div>
               <div className={`dropdown-content ${activeDropdowns[dropdown.id] ? 'active' : ''}`}>
-                <p>{dropdown.content}</p>
+                <p>{formatContent(dropdown.content)}</p>
+                <Latex>{dropdown.latex}</Latex>
               </div>
             </div>
           </React.Fragment>
