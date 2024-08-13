5754287ba08008d01e74788eaa31c758db5a626e
20shaked20 <shakedlevi32@gmail.com>
2024-08-13 18:52:26 +0300

    post request from the server now sends the data to the backend script !

--- Server/Backend/Algorithms.py
+++ Server/Backend/Algorithms.py
@@ -1,26 +1,50 @@
 # this file simply calls the algorithms from the networkZ library.
 # it processes the data given from the server side and then sends the results to the app.js side using the server
 # import networkz.
+import networkx as nx
 
+def create_nx_graph(data):
+    """
+    Creates a networkx graph from the provided data.
 
+    Parameters:
+    data (dict): A dictionary containing nodes and edges information.
+
+    Returns:
+    G (networkx.Graph): The created graph.
+    """
+    # Create an empty graph
+    G = nx.Graph()
+
+    # Add nodes to the graph
+    for node in data['nodes']:
+        G.add_node(node['id'], pos=(node['x'], node['y']))
+
+    # Add edges to the graph
+    for edge in data['edges']:
+        G.add_edge(edge['source'], edge['target'])
+
+    return G
 
 def run_algorithm(graph, selected_algorithm, source, target, budget):
-
-    import networkx as nx
-    G = nx.DiGraph()
-    G.add_nodes_from(graph['nodes'])
-    G.add_edges_from([(e['source'], e['target']) for e in graph['edges']])
+    print(graph)
+    G = create_nx_graph(graph)
 
     if selected_algorithm == "Non-Spreading MinBudget":
-        return non_spreading_minbudget(G, source, target)
+        print(selected_algorithm)
+        print(source)
+        print(target)
+        print(G.nodes)
+        print(G.edges)
+        # return non_spreading_minbudget(G, source, target)
     # Add more algorithm conditions here
     else:
         raise ValueError("Unknown algorithm selected")
 
-def non_spreading_minbudget(graph, source, target):
-    ans = Firefighter_Problem.non_spreading_minbudget(graph, source, target)
-    print(ans)
-    return ans
+# def non_spreading_minbudget(graph, source, target):
+#     ans = Firefighter_Problem.non_spreading_minbudget(graph, source, target)
+#     print(ans)
+#     return ans
 
 # Add more algorithm functions as needed
 
