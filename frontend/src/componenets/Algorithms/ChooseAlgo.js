b106efe2e72acf2d0cdb24f76e3f2195ec84001a
Yuval Bubnovsky <yuval.bubnov@gmail.com>
2024-08-28 15:55:58 +0300

    updated routes to accomodate static generation

--- frontend/src/componenets/Algorithms/ChooseAlgo.js
+++ frontend/src/componenets/Algorithms/ChooseAlgo.js
@@ -58,7 +58,7 @@ const ChooseAlgo = ({
           edges: edges.map(({ source, target }) => ({ source, target }))
         };
   
-        axios.post('https://almogyuvalshaked.csariel.xyz/run-algorithm', {
+        axios.post('/run-algorithm', {
           graph: graphData,
           selectedAlgorithm: localSelectedAlgorithm,
           sourceNode,
@@ -67,7 +67,7 @@ const ChooseAlgo = ({
         })
         .then(response => {
           console.log('Server response:', response.data);
-          return axios.get(`https://almogyuvalshaked.csariel.xyz/get-log/${response.data.logFilename}`, { responseType: 'text' })
+          return axios.get(`/get-log/${response.data.logFilename}`, { responseType: 'text' })
           .then(logResponse => {
             setAlgorithmResult({
               ...response.data,
