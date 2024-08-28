a6c99891b960a3d84685c41386a2938b9455c362
20shaked20 <shakedlevi32@gmail.com>
2024-08-28 16:03:55 +0300

    Changes to githupages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Toolbar from './componenets/Toolbar';
-import HomeScreen from './componenets/Home/HomeScreen';
-import Contact from './componenets/Home/Contact';
-import Algorithm from './componenets/Algorithms/AlgorithmsPage';
-import Information from './componenets/Home/Information';
+import Toolbar from './components/Toolbar';
+import HomeScreen from './components/Home/HomeScreen';
+import Contact from './components/Home/Contact';
+import Algorithm from './components/Algorithms/AlgorithmsPage';
+import Information from './components/Home/Information';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Toolbar /> {}
+        <Toolbar />
         <Routes>
-          <Route path="*" element={<HomeScreen/>} />
-          <Route path="/AlgorithmsPage" element={<Algorithm/>} />
-          <Route path="/Information" element={<Information/>} />
-          <Route path="/contact" element={<Contact/>} /> 
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/AlgorithmsPage" element={<Algorithm />} />
+          <Route path="/Information" element={<Information />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<HomeScreen />} />
         </Routes>
+        <footer>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
+            Learn React
           </a>
+        </footer>
       </div>
     </Router>
   );
